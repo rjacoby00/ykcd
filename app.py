9bffcdd70eac60ddb3beac521a59a380e7b82920
Ryan Jacoby <rjacoby00@icloud.com>
2017-11-03 09:52:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,47 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-	return '<h1>TEST</h1>'
+	return '''
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>RJ Notification Service</title>
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
+  <link rel="stylesheet" href="styles/index.css">
+</head>
+
+<body>
+
+  <header>
+    <h1>RJ Notify</h1>
+  </header>
+
+  <main>
+    <p>Welcome to RJ Notify.  Press the button below to join and get a key.</p>
+    <p>
+      <button disabled class="js-push-btn mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+        Enable Push RJ Notify
+      </button>
+    </p>
+    <section class="subscription-details js-subscription-details is-invisible">
+      <p>Subsribed, here's your key:</p>
+      <pre><code class="js-subscription-json"></code></pre>
+    </section>
+  </main>
+
+  <script src="scripts/main.js"></script>
+
+  <script src="https://code.getmdl.io/1.2.1/material.min.js"></script>
+</body>
+</html>
+	
+'''
 if __name__ == "__main__":
 	app.run()
